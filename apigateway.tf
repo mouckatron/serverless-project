6af2765faefb259475262215825e470f53484570
Graham Moucka <mouckatron@gmail.com>
2020-12-09 22:49:40 +0000

    Added integrations and fixed up some attributes

--- apigateway.tf
+++ apigateway.tf
@@ -5,26 +5,18 @@ resource "aws_api_gateway_rest_api" "main" {
   }
 }
 
-resource "aws_api_gateway_stage" "production" {
-  stage_name  = "production"
-  rest_api_id = aws_api_gateway_rest_api.main.id
-  tags = {
-    appname = var.appname
-  }
-}
-
-resource "aws_api_gateway_stage" "beta" {
-  stage_name = "beta"
-  tags = {
-    appname = var.appname
-  }
+resource "aws_api_gateway_authorizer" "main" {
+  name          = "${var.appname}-cognito-authorizer"
+  type          = "COGNITO_USER_POOLS"
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  provider_arns = aws_cognito_user_pool.frontend.arn
 }
 
 resource "aws_api_gateway_resource" "lambdas" {
   count = length(var.lambda_functions)
 
   rest_api_id = aws_api_gateway_rest_api.main.id
-  parent_id   = aws_api_gateway_rest_api.root_resource_id
+  parent_id   = aws_api_gateway_rest_api.main.root_resource_id
 
   path_part = var.lambda_functions[count.index].name
 
@@ -33,18 +25,7 @@ resource "aws_api_gateway_resource" "lambdas" {
   }
 }
 
-resource "aws_api_gateway_authorizer" "main" {
-  name          = "${var.appname}-cognito-authorizer"
-  type          = "COGNITO_USER_POOLS"
-  rest_api_id   = aws_api_gateway_rest_api.main.id
-  provider_arns = aws_cognito_user_pool.frontend.arn
-
-  tags = {
-    appname = var.appname
-  }
-}
-
-resource "aws_api_gateway_method" "any" {
+resource "aws_api_gateway_method" "lambdas-any" {
   count = length(var.lambda_functions)
 
   rest_api_id   = aws_api_gateway_rest_api.main.id
@@ -56,6 +37,42 @@ resource "aws_api_gateway_method" "any" {
   request_parameters = {
     "method.request.path.proxy" = true
   }
+
+  tags = {
+    appname = var.appname
+  }
+}
+
+resource "aws_api_gateway_integration" "lambdas" {
+  count = length(var.lambda_functions)
+
+  rest_api_id             = aws_api_gateway_rest_api.main.id
+  resource_id             = aws_api_gateway_resource.lambdas[count.index].id
+  http_method             = aws_api_gateway_method.lambdas-any[count.index].http_method
+  integration_http_method = "ANY"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda[count.index].invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "production" {
+  stage_name  = "production"
+  rest_api_id = aws_api_gateway_rest_api.main.id
+}
+
+resource "aws_api_gateway_stage" "production" {
+  stage_name    = "production"
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  deployment_id = aws_api_gateway_deployment.production.id
+
+  tags = {
+    appname = var.appname
+  }
+}
+
+resource "aws_api_gateway_stage" "beta" {
+  stage_name    = "beta"
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  deployment_id = aws_api_gateway_deployment.beta.id
 
   tags = {
     appname = var.appname
