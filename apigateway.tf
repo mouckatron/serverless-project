e6871674af5acc72a918231e610df85ab70e78ee
Graham Moucka <mouckatron@gmail.com>
2020-12-09 22:20:21 +0000

    Figuring out API Gateway

--- apigateway.tf
+++ apigateway.tf
@@ -1,19 +1,63 @@
-# resource "aws_apigatewayv2_api" "main" {
-#   name          = local.appname
-#   protocol_type = "HTTP"
+resource "aws_api_gateway_rest_api" "main" {
+  name = "${var.appname}-backend"
+  tags = {
+    appname = var.appname
+  }
+}
 
-#   tags = {
-#     appname = local.appname
-#   }
-# }
+resource "aws_api_gateway_stage" "production" {
+  stage_name  = "production"
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  tags = {
+    appname = var.appname
+  }
+}
 
-# resource "aws_apigatewayv2_integration" "order" {
-#   api_id           = aws_apigatewayv2_api.main.id
-#   integration_type = "AWS"
+resource "aws_api_gateway_stage" "beta" {
+  stage_name = "beta"
+  tags = {
+    appname = var.appname
+  }
+}
 
-#   connection_type           = "INTERNET"
-#   content_handling_strategy = "CONVERT_TO_TEXT"
-#   description               = "Lambda order"
-#   integration_method        = "ANY"
-#   integration_uri           = module.lambda_order.invoke_arn
-# }
+resource "aws_api_gateway_resource" "lambdas" {
+  count = length(var.lambda_functions)
+
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_rest_api.root_resource_id
+
+  path_part = var.lambda_functions[count.index].name
+
+  tags = {
+    appname = var.appname
+  }
+}
+
+resource "aws_api_gateway_authorizer" "main" {
+  name          = "${var.appname}-cognito-authorizer"
+  type          = "COGNITO_USER_POOLS"
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  provider_arns = aws_cognito_user_pool.frontend.arn
+
+  tags = {
+    appname = var.appname
+  }
+}
+
+resource "aws_api_gateway_method" "any" {
+  count = length(var.lambda_functions)
+
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.lambdas[count.index].id
+  http_method   = "ANY"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.main.id
+
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+
+  tags = {
+    appname = var.appname
+  }
+}
