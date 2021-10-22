b7efbac315b6c9ab0b265f21253a90ff74613ede
Graham Moucka <mouckatron@gmail.com>
2021-10-22 19:18:37 +0100

    update cognito auth output template

--- frontend-authentication-output-template.ts
+++ frontend-authentication-output-template.ts
@@ -1,6 +1,7 @@
 export const awsconfig = {
-  'aws_project_region': '${region}',
-  'aws_cognito_region': '${region}',
-  'aws_user_pools_id': '${pool_id}',
-  'aws_cognito_web_client_id': '${client_id}'
+  Auth: {
+    region: '${region}',
+    userPoolId: '${pool_id}',
+    userPoolWebClientId: '${client_id}'
+  }
 }
